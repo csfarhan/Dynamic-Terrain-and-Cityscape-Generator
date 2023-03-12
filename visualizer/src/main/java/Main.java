8331e18c5fee4d20eb3c41b67a0e5cb199241ffe
Sylvia Hurst <hursts4@mcmaster.ca>
2023-03-11 20:46:56 -0500

    A2 Walkthrough

--- visualizer/src/main/java/Main.java
+++ visualizer/src/main/java/Main.java
@@ -1,43 +1,22 @@
 import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
-import ca.mcmaster.cas.se2aa4.a2.visualizer.GraphicRenderer;
-import ca.mcmaster.cas.se2aa4.a2.visualizer.MeshDump;
-import ca.mcmaster.cas.se2aa4.a2.visualizer.SVGCanvas;
+import ca.mcmaster.cas.se2aa4.a2.visualizer.configuration.Configuration;
+import ca.mcmaster.cas.se2aa4.a2.visualizer.renderer.DebugRenderer;
+import ca.mcmaster.cas.se2aa4.a2.visualizer.renderer.GraphicRenderer;
+import ca.mcmaster.cas.se2aa4.a2.visualizer.SVGCanvasProvider;
+import ca.mcmaster.cas.se2aa4.a2.visualizer.renderer.Renderer;
 
 import java.awt.*;
-import java.io.IOException;
-import org.apache.commons.cli.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, ParseException {
-        // Option for debug mode
-        Options options = new Options();
-        Option debug = new Option("d", "debug", false, "Activate debug mode");
-        options.addOption(debug);
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = parser.parse(options, args);
-
-        // Extracting command line parameters
-        String input = args[0];
-        String output = args[1];
-        // Getting width and height for the canvas
-        Structs.Mesh aMesh = new MeshFactory().read(input);
-        double max_x = Double.MIN_VALUE;
-        double max_y = Double.MIN_VALUE;
-        for (Structs.Vertex v: aMesh.getVerticesList()) {
-            max_x = (Double.compare(max_x, v.getX()) < 0? v.getX(): max_x);
-            max_y = (Double.compare(max_y, v.getY()) < 0? v.getY(): max_y);
-        }
-        // Creating the Canvas to draw the mesh
-        Graphics2D canvas = SVGCanvas.build((int) Math.ceil(max_x), (int) Math.ceil(max_y));
-        GraphicRenderer renderer = new GraphicRenderer();
-        // Painting the mesh on the canvas
-        renderer.render(aMesh, canvas, cmd);
-        // Storing the result in an SVG file
-        SVGCanvas.write(canvas, output);
-        // Dump the mesh to stdout
-        MeshDump dumper = new MeshDump();
-        dumper.dump(aMesh);
+    public static void main(String[] args) throws Exception {
+        Configuration config = new Configuration(args);
+        Structs.Mesh aMesh = new MeshFactory().read(config.input());
+        Graphics2D canvas = SVGCanvasProvider.build(aMesh);
+        Renderer renderer = (config.debug()? new DebugRenderer(): new GraphicRenderer());
+        renderer.render(aMesh, canvas);
+        SVGCanvasProvider.write(canvas, config.output());
     }
+
 }
