cad37909a1cf48679dfbd2d1dc5d684533c0a2e4
csfarhan <89839649+csfarhan@users.noreply.github.com>
2023-03-13 05:40:41 -0400

    Different regions shapes implemented

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/specification/shape/CircleSpecification.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/specification/shape/CircleSpecification.java
@@ -11,14 +11,48 @@ public class CircleSpecification implements Shapable {
         List<Polygon> polygons, new_polygons;
         polygons = inputMesh.getPolygonsList();
         new_polygons = new ArrayList<>();
+        List<Vertex> centroidList = inputMesh.getVerticesList();
 
         //Ocean blue
-        Property color = Property.newBuilder().setKey("rgb_color").setValue("43,101,236").build();
+        Property color = Property.newBuilder().setKey("rgb_color").setValue("47,97,196").build();
+        Property color2 = Property.newBuilder().setKey("rgb_color").setValue("0,5,135").build();
+        Property color3 = Property.newBuilder().setKey("rgb_color").setValue("222,0,230").build();
 
-        //Fill ocean
+
+        // Find center of grid
+        double center_x = 1920/2;
+        double center_y = 1080/2;
+
+        // Iterate through and if distance is within radius then add to new_polygons (can edit radius)
         for (Polygon p : polygons) {
-            new_polygons.add(Polygon.newBuilder(p).addProperties(color).build());
+            double distance = Math.sqrt(Math.pow((centroidList.get(p.getCentroidIdx()).getX() - center_x), 2) + Math.pow((centroidList.get(p.getCentroidIdx()).getY() - center_y), 2));
+            Property distanceTemp = Property.newBuilder().setKey("distance").setValue(String.valueOf(distance)).build();
+            if (distance < 300) {
+                new_polygons.add(Polygon.newBuilder(p).addProperties(color).addProperties(distanceTemp).build());
+            } else if (distance > 500) {
+                new_polygons.add(Polygon.newBuilder(p).addProperties(color2).addProperties(distanceTemp).build());
+            } else {
+                new_polygons.add(Polygon.newBuilder(p).addProperties(distanceTemp).build());
+            }
         }
+
+        // Keep track of size to make sure doesnt reiterate over neighbouring polygons when size increases
+        int tempSize = new_polygons.size();
+        for (int i = 0; i < tempSize; i++){
+            // List of neighbours of each iteration
+            List<Integer> neighbours = new_polygons.get(i).getNeighborIdxsList();
+            // If polygon has no color
+            if (new_polygons.get(i).getPropertiesList().size() == 1){
+                // If polygon has a neighbour which has color
+                for (int j = 0; j < neighbours.size(); j++){
+                    if (new_polygons.get(neighbours.get(j)).getPropertiesList().size() > 1){
+                        new_polygons.add(Polygon.newBuilder(new_polygons.get(i)).addProperties(color3).build());
+                    }
+                }
+            }
+        }
+
+
 
         //Replace polygons with modified ones
         return Mesh.newBuilder(inputMesh).addAllPolygons(new_polygons).build();
